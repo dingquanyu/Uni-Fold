af3568b884830b8540c6c72921d91ccc5a0ff665
Geoffrey Yu <geoffrey.yu96@icloud.com>
2023-05-22 16:17:31 +0200

    remove unecessary logging

--- unifold/modules/common.py
+++ unifold/modules/common.py
@@ -8,7 +8,8 @@ import torch.utils.checkpoint
 
 from unicore.modules import LayerNorm
 from unicore.utils import tensor_tree_map
-
+import logging
+logger = logging.getLogger(__name__)
 
 class Linear(nn.Linear):
     def __init__(
